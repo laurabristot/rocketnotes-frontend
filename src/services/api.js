00d0f13dfa10d69beaf98f93c7678d70abddf901
Laura Bristot <107277969+laurabristot@users.noreply.github.com>
2023-02-16 17:34:13 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
 
+
+
+await axios.get(`https://rocketnotes-api-lfzv.onrender.com`, {
+  params: { /* Whatever data you want to send */ },
+  headers: {
+      'Content-Type': 'application/json',
+  }
+})
+
 export const api = axios.create({
-  baseURL: 'https://rocketnotesbylb.herokuapp.com/'
+  baseURL: 'https://rocketnotes-api-lfzv.onrender.com'
 })
